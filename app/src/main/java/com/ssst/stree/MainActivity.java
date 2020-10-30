6dd822016a82663e9cd2ece10b7a39df6440410b
Tejas Bele <tejasrbele@gmail.com>
2020-10-30 10:29:19 +0530

    ok

--- app/src/main/java/com/ssst/stree/MainActivity.java
+++ app/src/main/java/com/ssst/stree/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -21,6 +23,7 @@ import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Variables are initialized
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle toggle;
     NavigationView navigation;
@@ -29,60 +32,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
-        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
-        drawerLayout.addDrawerListener(toggle);
-        navigation = (NavigationView)findViewById(R.id.navigate);
-        toggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        setUpDrawerContent(navigation);
 
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS}, PackageManager.PERMISSION_GRANTED);
-    }
 
-    //navigation to different modules after selecting onr of the item from drawer
-    public void selectItemDrawer(MenuItem menuItem){
-        Fragment fragment = null;
-        //checking which item is selected from navigation drawer
-        Class fragmentClass;
-        switch (menuItem.getItemId()){
-            case R.id.awareness:
-                fragmentClass = Awareness.class;
-                break;
-            case R.id.skilldev:
-                fragmentClass = SkillDev.class;
-                break;
-            case R.id.financial:
-                fragmentClass = Financial.class;
-                break;
-            case R.id.problems:
-                fragmentClass = Problems.class;
-                break;
-            default:
-                fragmentClass = MainActivity.class;
-        }
-        menuItem.setChecked(true);   // Selected module will be highlighted
-        setTitle(menuItem.getTitle());
-        drawerLayout.closeDrawers();
-    }
+        //Assign Variables
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-    private void setUpDrawerContent(NavigationView navigation){
-        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                selectItemDrawer(menuItem);
-                return true;
-            }
-        });
-    }
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (toggle.onOptionsItemSelected(item))
-            return true;
-        return super.onOptionsItemSelected(item);
-    }
 
-    public void SoS(View view) {
+
+
+
+    /*public void SoS(View view) {
 
         String message = "Test";
         String number = "+919665308970";
@@ -91,7 +50,73 @@ public class MainActivity extends AppCompatActivity {
         mySms.sendTextMessage(number,null,message,null,null);
 
 
+    }*/
     }
 
+    public void ClickMenu(View view){
+        //Open Drawer
+        openDrawer(drawerLayout);
+    }
 
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        //Open Drawer Layout
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void ClickProfile(){
+        //Closes Drawer
+        closeDrawer(drawerLayout);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        //close drawer layout
+        //check condition
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            //When drawer is open
+            //Close the drawer
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public void ClickHome(View view){
+        //Recreate Activity
+        recreate();
+    }
+
+    public void ClickAwareness(View view){
+        //Redirect activity to Awareness
+        redirectActivity(this, Awareness.class);
+    }
+
+    public void ClickSkill(View view){
+        //redirect Activity
+        redirectActivity(this, SkillDevelopment.class);
+    }
+
+    public void ClickFinancial(View view){
+        //redirect Activity
+        redirectActivity(this, Financial.class);
+    }
+
+    public void ClickProblems(View view){
+        //redirect Activity
+        redirectActivity(this, Problems.class);
+    }
+
+    public static void redirectActivity(Activity activity, Class aClass) {
+        //Initialize Intent
+        Intent intent = new Intent(activity, aClass);
+        //set flag
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //start activity
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Close Drawer
+        closeDrawer(drawerLayout);
+    }
 }
+
