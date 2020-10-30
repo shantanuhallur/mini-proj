1fabcda708981c86fa3322caf1ae6f256053d034
Tejas Bele <tejasrbele@gmail.com>
2020-10-30 11:01:58 +0530

    Working Nav Bar

---
+++ app/src/main/java/com/ssst/stree/Awareness.java
@@ -0,0 +1,64 @@
+package com.ssst.stree;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Awareness extends AppCompatActivity {
+
+    //Initialize variable
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_awareness);
+
+        //Assign Variable
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void ClickMenu(View view){
+        //Open Drawer
+        MainActivity.openDrawer(drawerLayout);
+    }
+
+    public void ClickProfile(View view){
+        //Close Drawer
+        MainActivity.closeDrawer(drawerLayout);
+    }
+
+    public void ClickHome(View view){
+        //Redirect activity to Home
+        MainActivity.redirectActivity(this, MainActivity.class);
+    }
+
+    public void ClickAwareness(View view){
+        //Recreate this activity
+        recreate();
+    }
+
+    public void ClickSkill(View view){
+        //Redirect activity to SkillDevelopment
+        MainActivity.redirectActivity(this, SkillDevelopment.class);
+    }
+
+    public void ClickFinancial(View view){
+        //redirect Activity
+        MainActivity.redirectActivity(this, Financial.class);
+    }
+
+    public void ClickProblems(View view){
+        //redirect Activity
+        MainActivity.redirectActivity(this, Problems.class);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Close Drawer
+        MainActivity.closeDrawer(drawerLayout);
+    }
+}
---
+++ app/src/main/java/com/ssst/stree/SkillDevelopment.java
@@ -0,0 +1,63 @@
+package com.ssst.stree;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SkillDevelopment extends AppCompatActivity {
+
+    //Initialize Variable
+    DrawerLayout drawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skill_development);
+
+        //Assign Variable
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void ClickMenu(View view){
+        //Open Drawer
+        MainActivity.openDrawer(drawerLayout);
+    }
+
+    public void ClickProfile(View view){
+        //Close Drawer
+        MainActivity.closeDrawer(drawerLayout);
+    }
+
+    public void ClickHome(View view){
+        //Redirect activity to Home
+        MainActivity.redirectActivity(this, MainActivity.class);
+    }
+
+    public void ClickAwareness(View view){
+        //Redirect to Awareness
+        MainActivity.redirectActivity(this, Awareness.class);
+    }
+
+    public void ClickSkill(View view){
+        //Recreate this activity
+        recreate();
+    }
+
+    public void ClickFinancial(View view){
+        //redirect Activity
+        MainActivity.redirectActivity(this, Financial.class);
+    }
+
+    public void ClickProblems(View view){
+        //redirect Activity
+        MainActivity.redirectActivity(this, Problems.class);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Close Drawer
+        MainActivity.closeDrawer(drawerLayout);
+    }
+}
