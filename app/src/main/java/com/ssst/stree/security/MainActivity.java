caa6dac9409d3c1560da249927822eec5dae5ef7
shantanuhallur <shantanuhallur.imba@gmail.com>
2020-12-10 21:41:39 +0530

    fused

--- app/src/main/java/com/ssst/stree/security/MainActivity.java
+++ app/src/main/java/com/ssst/stree/security/MainActivity.java
@@ -2,20 +2,29 @@ package com.ssst.stree.security;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.ssst.stree.support.LoadingBar;
@@ -27,60 +36,131 @@ import com.ssst.stree.skilldev.SkillDevelopment;
 import com.ssst.stree.awareness.Awareness;
 import com.ssst.stree.financial.Financial;
 
+import java.util.concurrent.Executor;
+
 public class MainActivity extends AppCompatActivity {
-    float volume =  1;
+    private FusedLocationProviderClient fusedLocationClient;
+    Location location12;
+    float volume = 1;
     MediaPlayer player;
     MediaRecorder recorder;
     //Variables are initialized
     private DrawerLayout drawerLayout;
     private TextView profile;
     private LoadingBar loadingBar;
+    double lat, lon;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(checkSelfPermission(Manifest.permission.SEND_SMS)== PackageManager.PERMISSION_GRANTED )
-        {Toast.makeText(this,"Thank you for granting permissions for Lifesaving Security Module...!!!",Toast.LENGTH_LONG).show();}
-        else {
-            requestPermissions(new String[]{Manifest.permission.SEND_SMS}, 1);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+           Log.d("in if","of permission");
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            location12 = location;
+                            Log.d("security", location.toString()+ "*******************************************************************************************************************");
+                            Log.d("security",   "*******************************************************************************************************************");
+
+                        }
+                    }
+                });
+
+
+
+        if (checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED &&
+                checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Thank you for granting permissions for Lifesaving Security Module...!!!", Toast.LENGTH_LONG).show();
+        } else {
+            requestPermissions(new String[]{Manifest.permission.SEND_SMS, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
         }
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         profile = findViewById(R.id.profile);
     }
 
-
-     @RequiresApi(api = Build.VERSION_CODES.M)
-     public void SoS(View view) {
-
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void SoS(View view) {
 
 
 
-        //buzzer sound
-        if(player==null){
-            player = MediaPlayer.create(this,R.raw.buzzer);
-        }
-        player.start();
-        player.setLooping(true);
-        player.setVolume(volume,volume);
 
+        Log.d("security",  "*******************************************************************************************************************");
+        AsyncTaskRunner runner = new AsyncTaskRunner();
+        runner.execute();
 
-        if(checkSelfPermission(Manifest.permission.SEND_SMS)==PackageManager.PERMISSION_GRANTED) {
-            String message = "!!!@@@~~~EMERGENCY~~~@@@!!!\n" +
-                    "HELP ME ...!!! I AM IN DANGER AND PLEASE COME AS SOON AS YOU CAN !!! MY LOCATION IS :- \n" +
-                    "Latitude :- \n" +
-                    "Longitude :-";
-            String number = "+919665308970";
-
-            SmsManager mySms = SmsManager.getDefault();
-            mySms.sendTextMessage(number, null, message, null, null);
-        }
-        else{
-            requestPermissions(new String[]{Manifest.permission.SEND_SMS},1);
-        }
     }
+
+    private class AsyncTaskRunner extends  AsyncTask<String, String, String> {
+
+        private String resp;
+
+
+
+        @RequiresApi(api = Build.VERSION_CODES.M)
+        @Override
+        protected String doInBackground(String... params) {
+            //publishProgress("Sleeping..."); // Calls onProgressUpdate()
+            try {
+
+                //buzzer sound
+                if (player == null) {
+                    player = MediaPlayer.create(MainActivity.this, R.raw.buzzer);
+                }
+                player.start();
+                player.setLooping(true);
+                player.setVolume(volume, volume);
+
+
+                if (checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED &&
+                        checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+
+                    Log.d("hi", "hi");
+
+                    String message = "!!!@@@~~~EMERGENCY~~~@@@!!!\n" +
+                            "HELP ME ...!!! I AM IN DANGER AND PLEASE COME AS SOON AS YOU CAN !!! MY LOCATION IS :- \n" +
+                            "Latitude :- \n" + location12.toString()+
+                            "Longitude :-\n"  ;
+
+                    String number = "+919665308970";
+                    SmsManager mySms = SmsManager.getDefault();
+                    mySms.sendTextMessage(number, null, message, null, null);
+
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.SEND_SMS}, 1);
+                }
+
+
+        } catch(Exception e)
+
+        {
+            e.printStackTrace();
+            resp = e.getMessage();
+        }
+            return resp;
+    }
+
+
+//        @Override
+//        protected void onPostExecute(String result) {
+//            // execution of result of Long time consuming operation
+//            progressDialog.dismiss();
+//            finalResult.setText(result);
+//        }
+
+}
+
 
     public void ClickMenu(View view) {
         //Open Drawer
@@ -182,12 +262,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onResume () {
+    protected void onResume() {
         super.onResume();
         displayName();
     }
 
-    private void displayName(){
+    private void displayName() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             profile.setText(user.getEmail());
@@ -197,3 +277,72 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
+
+//    @RequiresApi(api = Build.VERSION_CODES.M)
+//    public void SoS(View view) {
+//
+
+
+//
+//
+//
+//        //buzzer sound
+//        if(player==null){
+//            player = MediaPlayer.create(this,R.raw.buzzer);
+//        }
+//        player.start();
+//        player.setLooping(true);
+//        player.setVolume(volume,volume);
+//
+//
+//        if(checkSelfPermission(Manifest.permission.SEND_SMS)==PackageManager.PERMISSION_GRANTED&&
+//                checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED) {
+
+//fusedLocationClient.getLastLocation()
+//        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+//@Override
+//public void onSuccess(Location location) {
+//        // Got last known location. In some rare situations this can be null.
+//        if (location != null) {
+//        lat = location.getLatitude();
+//        lon = location.getLongitude();
+//
+//
+////                                SmsManager smsManager = SmsManager.getDefault();
+////                                StringBuffer smsBody = new StringBuffer();
+////                                smsBody.append(message);
+////                                smsBody.append("http://maps.google.com?q=");
+////                                smsBody.append(location.getLatitude());
+////                                smsBody.append(",");
+////                                smsBody.append(location.getLongitude());
+////                                smsManager.sendTextMessage(phoneNumber, null, smsBody.toString(), null, null);
+////                             double mylat= location.getLatitude();
+////                                double mylon = location.getLongitude();
+//        Log.d("security", String.valueOf(lat) + "*******************************************************************************************************************");
+//        Log.d("security", String.valueOf(lon) + "*******************************************************************************************************************");
+//
+//
+//        }
+//        }
+//        });
+//
+//            Log.d("hi","hi");
+//
+//            String message = "!!!@@@~~~EMERGENCY~~~@@@!!!\n" +
+//                    "HELP ME ...!!! I AM IN DANGER AND PLEASE COME AS SOON AS YOU CAN !!! MY LOCATION IS :- \n" +
+//                    "Latitude :- \n" +lat+
+//                    "Longitude :-\n"+lon;
+//
+//            String number = "+919665308970";
+//            SmsManager mySms = SmsManager.getDefault();
+//            mySms.sendTextMessage(number, null, message, null, null);
+//
+//        }
+//
+//
+//
+//
+//        else{
+//            requestPermissions(new String[]{Manifest.permission.SEND_SMS},1);
+//        }
+//        }
